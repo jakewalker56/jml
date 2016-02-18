f19ab0ded0f7e7c1a51f2573025868b7d63c3155
Jacob Walker <jakewalker@chicagobooth.edu>
2016-02-18 15:47:32 -0600

    initial commit

---
+++ R/convert_to_numeric.R
@@ -0,0 +1,36 @@
+#' Convert To Numeric
+#'
+#' This function converts a vector or data frame to numeric values
+#' @param d the data to be converted
+#' @keywords numeric convert
+#' @export
+#' @examples
+#' convert_to_numeric(data.frame(y=array("1", "2", "3")))
+
+atomic_convert_to_numeric <- function(d){
+  if(!is.numeric(d)){
+    if(is.factor(d)) {
+      #if the value is NA, set it to 0
+      d[is.na(d)] <- "0"
+      #if factor, convert it to its char representation before converting to numeric. This at least supports factored integer representations
+      d <- as(d, "character")
+    }
+    d <- as(d,"numeric")
+  } else {
+    #this is numeric, but it could still have NA's
+    d[is.na(d)] <- 0
+  }
+  return(d)
+}
+convert_to_numeric <- function (d){
+  if(is.atomic(d)) {
+    return(atomic_convert_to_numeric(d))
+  } else {
+    for(di in colnames(d)) {
+      #not sure if you can have a frame with a frame inside it, but I guess we'll support it with a recursive call?
+      #this will break if convert_to_numeric doesn't simplify to an atomic eventually
+      d[,di] <- convert_to_numeric(d[,di])
+    }
+ }
+  return(d)
+}
---
+++ R/print_formula.R
@@ -0,0 +1,14 @@
+print_formula <- function(coefs) {
+  myFormula = ""
+  for(i in 1:length(coefs)) {
+    if(names(coefs)[i] != "(Intercept)") {
+      myFormula = paste(myFormula, names(coefs)[i], sep="")
+      myFormula = paste(myFormula, "*", sep="")
+    }
+    myFormula = paste(myFormula, coefs[i], sep="")
+    if(i != length(coefs)) {
+      myFormula = paste(myFormula, " + ", sep="")
+    }
+  }
+  return(myFormula)
+}
